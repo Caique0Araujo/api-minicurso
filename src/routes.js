4d5d78789ebfd9ffe32d8622c39eed21ab6d1aa1
Caique0Araujo <caique.ara19@gmail.com>
2022-11-10 14:09:15 -0300

    done

--- src/routes.js
+++ src/routes.js
@@ -2,24 +2,24 @@ const express = require('express');
 const router = express.Router();
 const user = require('./models/user');
 
-router.post('/create', async (req, res)=>{
+router.post('/criar', async (req, res)=>{
   const {nome, login, senha} = req.body;
-  const novo_usuario = await user.create(nome, login, senha);
+  const novo_usuario = await user.create({nome, login, senha});
   res.json(novo_usuario).status(201);
 })
 
-router.get('/all', async (req, res)=>{
+router.get('/todos', async (req, res)=>{
   const users = await user.findAll({raw: true});
-  res.json(user).status(200);
+  res.json(users).status(200);
 })
 
-router.put('/update', async (req, res)=>{
+router.put('/atualizar', async (req, res)=>{
   const {id, nome, login, senha} = req.body;
   const update = await user.update({nome, login, senha}, { where: { id: id } });
-  res.json(update).status(200);
+  res.json({message: "Usuário atualizado"}).status(200);
 })
 
-router.delete('/delete/:id', async (req, res)=>{
+router.delete('/deletar/:id', async (req, res)=>{
   const id = req.params.id;
   user.destroy({where: {id: id}});
   res.json({message: 'Usuário removido'}).status(200);
